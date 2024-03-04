9a68fd8825b984b38039aa56f07e1c2993f95578
Angelo Viero <angelosviero@gmail.com>
2024-03-04 16:48:56 -0300

    Corrigindo o exercício 8

--- src/Ex8.java
+++ src/Ex8.java
@@ -15,7 +15,6 @@ de 8% sobre R$ 1000.00 + 18% sobre R$ 2.00, o que resulta em R$ 80.36 no total. 
 duas casas decimais.
  */
 
-
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -23,5 +22,25 @@ public class Ex8 {
 
     public static void main(String[] args){
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Salário: R$");
+        double salario = sc.nextDouble();
+        double imposto = 0.0;
+
+        if(salario <= 2000.00){
+            System.out.println("Isento");
+        }else if(salario <= 3000.00){
+            imposto = (salario - 2000.00) * 0.08;
+        }else if(salario <= 4500.00){
+            imposto = 1000.00 * 0.08 + (salario - 3000.00) * 0.18;
+        }else{
+            imposto = 1000.00 * 0.08 + 1500.00 * 0.18 + (salario - 4500.00) * 0.28;
+        }
+
+        System.out.printf("Imposto: R$%.2f%n", imposto);
+
+        sc.close();
     }
 }
